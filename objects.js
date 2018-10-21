a284f31ad31fb2f5dd7d58528de9124189a74338
Taimur Shah <tshah@scu.edu>
2018-10-21 23:47:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,4 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName)
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
+}
+
